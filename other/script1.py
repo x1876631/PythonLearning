1c36fabb56e65a098babb0f73fe8f9b10a82390b
xuye <xuye@chunyu.me>
2017-09-18 20:37:24 +0800

    python入门操作

---
+++ other/script1.py
@@ -0,0 +1,11 @@
+#! /usr/local/bin/python
+# coding=utf-8
+# 第一行使用#!声明python解释器路径,操作系统使用它来找到解释器
+import sys
+
+print sys.platform
+print 2 ** 100
+x = 'Samp!'
+print x * 8
+
+print 'The Bright Side ' + 'of Life...中文可以吗?'
